dda0123ca44548878136d8281f73795ae5e6a499
msyedniz <msyedniz@uncc.edu>
2020-12-03 10:38:07 -0500

    GraphlQL and mongo

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://ralph_user:Ga1uq9HQU0yMlp9N@cluster0.t2wfn.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'encode the token check key'
+}
---
+++ index.js
@@ -0,0 +1,20 @@
+const {ApolloServer} = require('apollo-server');
+const mongoose = require('mongoose');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const {MONGODB} = require('./config.js');
+
+const server = new ApolloServer ({
+    typeDefs,
+    resolvers
+})
+mongoose
+.connect(MONGODB, {useNewUrlParser: true})
+    .then(
+    ()=>{
+        console.log('mongo connected');
+        return server.listen({ port: 5000});
+    }).then((res)=> {
+    console.log(`Server is running at ${res.url}`);
+})
