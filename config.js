ea43bdcb6acf64aca634f319dbb42dc71b3fdbfc
Md Hussain Musthaq S N <54637394+sanzder24@users.noreply.github.com>
2020-12-03 10:39:07 -0500

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGODB: 'mongodb+srv://ralph_user:Ga1uq9HQU0yMlp9N@cluster0.t2wfn.mongodb.net/merng?retryWrites=true&w=majority',
-    SECRET_KEY: 'encode the token check key'
-}
